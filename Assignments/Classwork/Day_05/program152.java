71cfc21bd237d4339b013dffc87cdc0b82811290
Saurabh kanade <saurabhkanade9922@gmail.com>
2025-11-29 11:27:11 +0530

    Logic of pattern printing
    By using row and column matrix printing

---
+++ Assignments/Classwork/Day_05/program152.java
@@ -0,0 +1,36 @@
+// input : 5
+// output : *  *  *  *  *  *
+
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iNo)
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= iNo; iCnt++)
+        {
+            System.out.print("*\t");
+        }
+        System.out.println();    // new line
+    }
+
+}
+
+class program152
+{
+    public static void main(String A[])
+    {
+
+        Scanner sobj = new Scanner(System.in);
+        int iValue = 0;
+
+        System.out.println("Enter the frequency : ");
+        iValue = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue);
+    }
+}
+
