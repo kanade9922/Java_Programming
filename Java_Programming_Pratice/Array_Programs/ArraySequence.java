5349698c49d66c07b5d9408accd709f0f5a2b2fb
Saurabh kanade <saurabhkanade9922@gmail.com>
2026-04-25 23:48:02 +0530

    Add files via upload

---
+++ Java_Programming_Pratice/Array_Programs/ArraySequence.java
@@ -0,0 +1,15 @@
+
+class ArraySequence
+{
+    public static void main(String A[])
+    {
+        int Arr[] = {10,20,30,40};
+
+        System.out.println("Number of elements : "+Arr.length);
+
+        System.out.println(Arr[0]);
+        System.out.println(Arr[1]);
+        System.out.println(Arr[2]);
+        System.out.println(Arr[3]);
+    }
+}
